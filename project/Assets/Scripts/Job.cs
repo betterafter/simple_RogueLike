062cedcc48ea6b1e8c4ba1a42f5fe6b7ff1abdfa
KO <57106957+KODAESEOK@users.noreply.github.com>
2021-01-17 21:19:19 +0900

    class
    class

---
+++ project/Assets/Scripts/Job.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class Job : MonoBehaviour
+{
+    protected string jobName;
+    public abstract void doAttack();
+    public abstract void doSkill1(bool typeCheck);
+    public abstract void doSkill2(bool typeCheck);
+    public abstract Job checkJobChange(int health, int ad, int md, int aspeed, int armor);
+    public abstract bool weaponCheck(WeaponTypes weapon);
+}
